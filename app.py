e8a2fb53850ff9d00c5e0cecd6bf879d635b8ee3
BenDavis71 <bendavis71@Knights.ucf.edu>
2020-09-09 18:57:41 -0500

    Reading .csv from Github

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ import requests
 
 
 st.title('Six Degrees of College Football')
-st.markdown('_Data courtesy of BorderingOnWisdom | Inspiration via @__DapperDan_')
+st.markdown('_Data courtesy of BorderingOnWisdom | Inspiration from @__DapperDan_')
 
 @st.cache
 def getData():
-    df = pd.read_csv('CFB Coaches.csv')
+    url = 'https://raw.githubusercontent.com/BenDavis71/SixDegreesOfCollegeFootball/master/CFB%20Coaches.csv'
+    df = pd.read_csv(url)
     return df
 
 df = getData()
@@ -53,7 +54,7 @@ graph = graphviz.Digraph()
 for _ in path:
     if i + 1 == len(path):
         break
-    st.write('{0} was on the {1} team with {2}'.format(path[i], path[i+1], path[i+2]))
+    #st.write('{0} was on the {1} team with {2}'.format(path[i], path[i+1], path[i+2]))
     graph.edge(path[i], path[i+2], label=path[i+1])
     i+=2
 
