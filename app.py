a0ed43cc10b4301fe72fca26c8b27abb453a0710
BenDavis71 <69690328+BenDavis71@users.noreply.github.com>
2024-01-29 23:54:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,9 @@ for _ in path:
 
 st.graphviz_chart(str(graph))
 
+path = nx.all_shortest_paths(G,source=a,target=b)
+st.graphviz_chart(str(graph))
+
 
 st.markdown('___')
 st.markdown('Created by [Ben Davis](https://github.com/BenDavis71/)')
